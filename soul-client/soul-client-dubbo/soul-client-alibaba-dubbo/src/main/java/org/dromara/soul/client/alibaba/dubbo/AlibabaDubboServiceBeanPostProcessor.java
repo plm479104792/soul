a5aa1c9a6f89e1d949573708a742152341d768e4
xvweixin <xvweixin@webuy.ai>
2021-03-11 17:31:42 +0800

    fix can not get SoulDubboClass.class in ApacheDubboServiceBeanPostProcessor.postProcessAfterInitialization

--- soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
+++ soul-client/soul-client-dubbo/soul-client-alibaba-dubbo/src/main/java/org/dromara/soul/client/alibaba/dubbo/AlibabaDubboServiceBeanPostProcessor.java
@@ -15,6 +15,7 @@ import org.dromara.soul.client.common.utils.OkHttpTools;
 import org.dromara.soul.client.dubbo.common.annotation.SoulDubboClient;
 import org.dromara.soul.client.dubbo.common.config.DubboConfig;
 import org.dromara.soul.client.dubbo.common.dto.MetaDataDTO;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.util.ClassUtils;
@@ -27,13 +28,13 @@ import org.springframework.util.ReflectionUtils;
  */
 @Slf4j
 public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
-    
+
     private DubboConfig dubboConfig;
-    
+
     private ExecutorService executorService;
-    
+
     private final String url;
-    
+
     public AlibabaDubboServiceBeanPostProcessor(final DubboConfig dubboConfig) {
         String contextPath = dubboConfig.getContextPath();
         String adminUrl = dubboConfig.getAdminUrl();
@@ -45,7 +46,7 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
         url = dubboConfig.getAdminUrl() + "/soul-client/dubbo-register";
         executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     }
-    
+
     @Override
     public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
         if (bean instanceof ServiceBean) {
@@ -53,7 +54,7 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
         }
         return bean;
     }
-    
+
     private void handler(final ServiceBean serviceBean) {
         Class<?> clazz = serviceBean.getRef().getClass();
         if (ClassUtils.isCglibProxyClass(clazz)) {
@@ -65,6 +66,9 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
                 return;
             }
         }
+        if (AopUtils.isJdkDynamicProxy(serviceBean.getRef())) {
+            clazz = AopUtils.getTargetClass(serviceBean.getRef());
+        }
         final Method[] methods = ReflectionUtils.getUniqueDeclaredMethods(clazz);
         for (Method method : methods) {
             SoulDubboClient soulDubboClient = method.getAnnotation(SoulDubboClient.class);
@@ -73,7 +77,7 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
             }
         }
     }
-    
+
     private String buildJsonParams(final ServiceBean serviceBean, final SoulDubboClient soulDubboClient, final Method method) {
         String appName = dubboConfig.getAppName();
         if (appName == null || "".equals(appName)) {
@@ -102,9 +106,9 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
                 .enabled(soulDubboClient.enabled())
                 .build();
         return OkHttpTools.getInstance().getGosn().toJson(metaDataDTO);
-        
+
     }
-    
+
     private String buildRpcExt(final ServiceBean serviceBean) {
         MetaDataDTO.RpcExt build = MetaDataDTO.RpcExt.builder()
                 .group(serviceBean.getGroup())
@@ -114,9 +118,9 @@ public class AlibabaDubboServiceBeanPostProcessor implements BeanPostProcessor {
                 .timeout(serviceBean.getTimeout())
                 .build();
         return OkHttpTools.getInstance().getGosn().toJson(build);
-        
+
     }
-    
+
     private void post(final String json) {
         try {
             String result = OkHttpTools.getInstance().post(url, json);
